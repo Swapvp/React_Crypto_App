7e1e379661ea7d942aa76102cdef3e0e30a0e828
Swapvp <swapneshpawar1994@gmail.com>
2024-06-17 11:11:35 +0530

    crypto_app

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Crypto from "./Components/Crypto";
+
+function App() {
+  return <Crypto />;
+}
+
+export default App;
---
+++ src/Components/Crypto.jsx
@@ -0,0 +1,93 @@
+import Axios from "axios";
+import { useEffect, useState } from "react";
+
+function Crypto() {
+  // Setting up the initial states using
+  // react hook 'useState'
+  const [search, setSearch] = useState("");
+  const [crypto, setCrypto] = useState([]);
+
+  async function fetchCoinData() {
+    const url = `https://api.coinstats.app/public/v1/coins?skip=0&limit=100&currency=INR`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      setCrypto(data.data.coins);
+    } catch (error) {
+      console.error(`Error fetching data: ${error}`);
+    }
+  }
+
+  // Fetching crypto data from the API only
+  // once when the component is mounted
+  useEffect(() => {
+    // Axios.get(
+    //   `https://api.coinstats.app/public/v1/coins?skip=0&limit=100¤cy=INR`
+    // ).then((res) => {
+    //   setCrypto(res.data.coins);
+    // });
+    fetchCoinData();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>All Cryptocurrencies</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <table>
+        <thead>
+          <tr>
+            <td>Rank</td>
+            <td>Name</td>
+            <td>Symbol</td>
+            <td>Market Cap</td>
+            <td>Price</td>
+            <td>Available Supply</td>
+            <td>Volume(24hrs)</td>
+          </tr>
+        </thead>
+        {/* Mapping all the cryptos */}
+        <tbody>
+          {/* Filtering to check for the searched crypto */}
+          {crypto
+            .filter((val) => {
+              return val.name.toLowerCase().includes(search.toLowerCase());
+            })
+            .map((val, id) => {
+              return (
+                <>
+                  <tr id={id}>
+                    <td className="rank">{val.rank}</td>
+                    <td className="logo">
+                      <a href={val.websiteUrl}>
+                        <img src={val.icon} alt="logo" width="30px" />
+                      </a>
+
+                      <p>{val.name}</p>
+                    </td>
+                    <td className="symbol">{val.symbol}</td>
+                    <td>₹{val.marketCap}</td>
+                    <td>₹{val.price.toFixed(2)}</td>
+                    <td>{val.availableSupply}</td>
+                    <td>{val.volume.toFixed(0)}</td>
+                  </tr>
+                </>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Crypto;
